58b19c4a4774a8f5dcb2623bd88902b28b6dd46b
bohemestudio <contact@bohemestudio.com>
2013-09-06 22:35:10 +0200

    Viewport meta updated mobile version

--- bohemestudio3.0/header.php
+++ bohemestudio3.0/header.php
@@ -7,8 +7,7 @@
 
 	<head>
 
-
-		<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
